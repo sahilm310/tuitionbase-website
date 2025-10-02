523f447cbf480e5876dc64e5d796edddc8b382f4
Sahil <sa.miah@reply.com>
2025-10-02 23:33:38 +0100

    Initial Commit - Site Working

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import relativeLinks from 'astro-relative-links';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [relativeLinks()]
+});
---
+++ src/config/constants.ts
@@ -0,0 +1,5 @@
+// Global constants that can be imported anywhere
+export const phoneNumber = "0203 633 4822";
+export const phoneNumberLink = phoneNumber.replaceAll(" ", "");
+export const email = "info@tuitionbase.co.uk";
+export const whatsappNumber = "447932391843";
