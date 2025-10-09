1b308205e4a09d9a6b916c10f2a5053c38dc0cab
Sahil <sa.miah@reply.com>
2025-10-09 21:06:54 +0100

    Add Favicons, meta tags and ogg

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,4 +2,5 @@
 export const phoneNumber = "0203 633 4822";
 export const phoneNumberLink = phoneNumber.replaceAll(" ", "");
 export const email = "info@tuitionbase.co.uk";
+export const websiteUrl = "https://tuitionbase.co.uk";
 export const whatsappNumber = "447932391843";
